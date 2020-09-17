0b1621ee48ba650356f8761e67c65385651f5e80
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-09-18 00:53:58 +0530

    quiz screen added...will complete the ui todday all the ui

--- quizapp/lib/src/pages/quiz/quiz.dart
+++ quizapp/lib/src/pages/quiz/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Quiz extends StatefulWidget {
   static const routeName = "/quiz";
@@ -10,11 +11,75 @@ class _QuizState extends State<Quiz> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text(
-          "This is my quiz!"
-        ),
+          appBar: AppBar(
+            title: Text("THis is title"),
+          ),
+          body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.blue[200],Colors.greenAccent])),
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height*0.25,
+                
+                child:Text("this is a container")
+              ),
+              Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height*0.25,
+                
+                child:Text("this is a container")
+              ),
+              // Container(
+              //   width: double.infinity,
+              //   height: MediaQuery.of(context).size.height*0.25,
+                
+              //   child:Text("this is a container") 
+              // ),
+              Container(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width*0.5,
+                          height:MediaQuery.of(context).size.height *0.2,
+                          child: Text("hey"),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width*0.5,
+                          height:MediaQuery.of(context).size.height *0.2,
+                          child: Text("Man"),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width*0.5,
+                          height:MediaQuery.of(context).size.height *0.2,
+                          child: Text("whats"),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width*0.5,
+                          height:MediaQuery.of(context).size.height *0.2,
+                          child: Text("up"),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+              
+              
+            ],
+          ),
       ),
     );
   }
+
 }
