485e0fd90b687796a00038fc1dd6bf90ffa5b2ac
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-09-17 22:50:33 +0530

    layouts added and button added as well

--- quizapp/lib/src/pages/quiz/startquiz.dart
+++ quizapp/lib/src/pages/quiz/startquiz.dart
@@ -20,15 +20,61 @@ class _StartquizState extends State<Startquiz> {
           child: Column(
             children: [
               Container(
-
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height*0.25,
+                color: Colors.red,
+                child:Text("this is a container")
               ),
               Container(
-                
-              )
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height*0.25,
+                color: Colors.grey,
+                child:Text("this is a container")
+              ),
+              Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height*0.25,
+                color: Colors.red[100],
+                child:Text("this is a container") 
+              ),
+              Container(
+                 width: double.infinity,
+                height: MediaQuery.of(context).size.height*0.25,
+                padding: EdgeInsets.only(top: 3, left: 3),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  border: Border(
+                    bottom: BorderSide(color: Colors.black),
+                    top: BorderSide(color: Colors.black),
+                    left: BorderSide(color: Colors.black),
+                    right: BorderSide(color: Colors.black),
+                  )
+                ),
+                child: MaterialButton(
+                  minWidth: double.infinity,
+                  height: 60,
+                  onPressed: () {
+                      
+                  },
+                  color: Colors.greenAccent,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)
+                  ),
+                  child: Text("Start Quiz", style: TextStyle(
+                    fontWeight: FontWeight.w600, 
+                    fontSize: 18
+                  ),),
+                ),
+                )
             ],
           ),
       ),
     );
   }
 }
+
+
+
+
 
