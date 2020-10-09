d3c19761e0f5d09f2f7d64fc4af3836371b36f2b
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-10-09 13:14:04 +0530

    added the topbar in the quiz homepage

--- quizapp/lib/src/pages/home/home_page.dart
+++ quizapp/lib/src/pages/home/home_page.dart
@@ -431,6 +431,63 @@ class _HomePageState extends State<HomePage> {
     return BottomNavigationBarItem(icon: Icon(icon), title: Text(""));
   }
 
+  Widget _header(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return ClipRRect(
+      borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
+      child: Container(
+          height: 120,
+          width: width,
+          decoration: BoxDecoration(
+            color: LightColor.black,
+          ),
+          child: Stack(
+            fit: StackFit.expand,
+            alignment: Alignment.center,
+            children: <Widget>[
+              Positioned(
+                  top: 10,
+                  right: -120,
+                  child: _circularContainer(300, Colors.grey)),
+              Positioned(
+                  top: -60,
+                  left: -65,
+                  child: _circularContainer(width * .5, Colors.yellow[300])),
+              Positioned(
+                  top: -230,
+                  right: -30,
+                  child: _circularContainer(width * .7, Colors.transparent,
+                      borderColor: Colors.white38)),
+              Positioned(
+                  top: 50,
+                  left: 0,
+                  child: Container(
+                      width: width,
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: Stack(
+                        children: <Widget>[
+                          Icon(
+                            Icons.keyboard_arrow_left,
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                          Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                "My Class",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w500),
+                              ))
+                        ],
+                      ))),
+            ],
+          )),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     width = MediaQuery.of(context).size.width;
@@ -468,7 +525,7 @@ class _HomePageState extends State<HomePage> {
             child: Container(
           child: Column(
             children: <Widget>[
-              // _header(context),
+              _header(context),
               SizedBox(height: 20),
               _categoryRow("Featured", LightColor.orange, LightColor.orange),
               _featuredRowA(),
